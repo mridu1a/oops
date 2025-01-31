9ddaa54122541073f37dfac2244e03bd05f42625
mridula <mridulajb1111@gmail.com>
2025-01-31 23:32:41 +0530

    cgange made

---
+++ files/Sample.java
@@ -0,0 +1,11 @@
+package files;
+public class Sample
+{
+public static void main(String[] args)
+{
+// Using Console to input data from user
+String name = System.console().readLine();
+System.out.println(name);
+}
+}
+//**change made**
